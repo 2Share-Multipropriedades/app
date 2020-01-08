618d8ebf078973a370cb3833220d5d3bae0c3c82
dudhavix <dudacsvix@gmail.com>
2020-01-08 12:47:01 -0300

    Alteração na validação do número de telefone

--- public/codigoJS/promoter.js
+++ public/codigoJS/promoter.js
@@ -14,35 +14,7 @@ $('document').ready(function () {
 })
 
 
-
-function validar_numero_celular() {
-    if (($('#celular').val().length) < 15) {
-        Swal.fire({
-            icon: 'warning',
-            text: 'Preencha o número de celular adequadamente'
-        })
-    }
-    var split1 = $('#celular').val().split(' ');
-    var split2 = split1[1].split('-')
-    var n1 = split2[0].length;
-    var n2 = split2[1].length;
-    if (n1 < 5 || n1 > 5) {
-        Swal.fire({
-            icon: 'warning',
-            text: 'Isso não é um número de celular. Tente outro'
-        })
-    }
-    else if (n2 < 4 || n2 > 4) {
-        Swal.fire({
-            icon: 'warning',
-            text: 'Isso não é um número de celular. Tente outro'
-        })
-    }
-}
-
-
-
-$("#promoter_btn_registro").on("click", function (e) {
+$("#login").on("click", function (e) {
     e.preventDefault();
 
     var email = $('#email').val();
@@ -165,73 +137,6 @@ $("#promoter_btn_registro").on("click", function (e) {
 })
 
 
-
-function carregar_estados() {
-    //var id_estado = $('#estado').val();
-    $.ajax({
-        type: 'GET',
-        url: '/carregar_estados',
-        dataType: 'json',
-        contentType: 'application/x-www-form-urlencoded',
-
-    }).done(function (resposta) {
-        $(resposta.estados).each(function (index, element) {
-            $('<option>').val(element.id).text(element.estado).appendTo($('#estado'));
-        });
-    })
-}
-
-
-
-function carregar_cidades() {
-    var id_estado = $('#estado').val();
-
-    $('#cidade').val("").text("");
-    $('<option>').val("").text("Selecione").appendTo('#cidade');
-
-    $.ajax({
-        type: 'GET',
-        url: '/carregar_cidades',
-        dataType: 'json',
-        contentType: 'application/x-www-form-urlencoded',
-
-    }).done(function (resposta) {
-        $(resposta.cidades).each(function (index, element) {
-            // de acordo com o id do estado do select 
-            if (id_estado == element.id_estado) {
-                $('<option>').val(element.id).text(element.cidade).appendTo($('#cidade'));
-            }
-        });
-    })
-}
-
-
-
-function carregar_paceiros() {
-    var id_estado = $('#estado').val();
-    var id_cidade = $('#cidade').val();
-
-    $('#parceiro').val("").text("");
-    $('<option>').val("").text("Selecione").appendTo('#parceiro');
-
-    $.ajax({
-        type: 'GET',
-        url: '/carregar_parceiros',
-        dataType: 'json',
-        contentType: 'application/x-www-form-urlencoded',
-
-    }).done(function (resposta) {
-        $(resposta.parceiros).each(function (index, element) {
-            // de acordo com o id do estado e da cidade do select 
-            if ((id_estado == element.id_estado) && (id_cidade == element.id_cidade)) {
-                $('<option>').val(element.id).text(element.nome).appendTo($('#parceiro'));
-            }
-        });
-    })
-}
-
-
-
 $("#sair").on("click", function (e) {
     e.preventDefault();
 
@@ -268,7 +173,7 @@ $("#sair").on("click", function (e) {
 })
 
 
-$('#voltar').on('click', function(e){
+$('#voltar').on('click', function (e) {
     window.location.href = '/promoter-menu'
 })
 
@@ -288,107 +193,100 @@ $("#enviar_SMS").on("click", function (e) {
             confirmButtonText: 'Ok',
         })
     } else {
-        //tirar os caracteres especiais do telefone
         var numero = telefone.substr(1, 2) + telefone.substr(5, 5) + telefone.substr(11, 4);
-        //FIM tirar os caracteres especiais do telefone
+        if (!validar_numero_celular(numero)) {
+            Swal.fire({
+                icon: 'warning',
+                text: 'Preencha o número de celular adequadamente'
+            })
+        } else {
+            document.getElementById("enviar_SMS").disabled = true;
 
+            $.ajax({
+                type: 'POST',
+                url: '/salvar-lead',
+                data: {
+                    id_parceiro: localStorage.getItem('parceiro'),
+                    id_promoter: localStorage.getItem('promoter'),
+                    promoter_crm: localStorage.getItem('crm'),
+                    nome: nome,
+                    numero_celular: numero,
+                    nome_promoter: localStorage.getItem('nome_promoter'),
+                },
+                dataType: 'json',
+                contentType: 'application/x-www-form-urlencoded',
 
-        $.ajax({
-            type: 'POST',
-            url: '/salvar-lead',
-            data: {
-                id_parceiro: localStorage.getItem('parceiro'),
-                id_promoter: localStorage.getItem('promoter'),
-                promoter_crm: localStorage.getItem('crm'),
-                nome: nome,
-                numero_celular: numero,
-                nome_promoter: localStorage.getItem('nome_promoter'),
-            },
-            dataType: 'json',
-            contentType: 'application/x-www-form-urlencoded',
+            }).done(function (resposta) {
+                switch (resposta.status) {
+                    case 'Erro_Lead':
+                        Swal.fire({
+                            icon: 'error',
+                            text: 'Algo deu errado ao salvar o Lead, tente novamente mais tarde...',
+                            allowOutsideClick: false,
+                            allowEscapeKey: false,
+                            confirmButtonText: 'Ok',
+                        })
+                        break;
 
-        }).done(function (resposta) {
-            switch (resposta.status) {
-                case 'Erro_Lead':
-                    Swal.fire({
-                        icon: 'error',
-                        text: 'Algo deu errado ao salvar o Lead, tente novamente mais tarde...',
-                        allowOutsideClick: false,
-                        allowEscapeKey: false,
-                        confirmButtonText: 'Ok',
-                    })
-                    break;
+                    case 'Lead_OK':
+                        var codigo_lead = btoa(resposta.id_lead)
+                        localStorage.setItem('id_lead', codigo_lead)
+                        //Mensagem
+                        var mensagem = 'Acesse o link abaixo, complete seu cadastro. Viva uma grande experiencia com o Residence Club at the Hard Rock Hotel. http://lead.vcisa.com/lead/' + codigo_lead
 
-                case 'Lead_OK':
-                    var codigo_lead = btoa(resposta.id_lead)
-                    localStorage.setItem('id_lead', codigo_lead)
-                    //Mensagem
-                    var mensagem = 'Acesse o link abaixo, complete seu cadastro. Viva uma grande experiencia com o Residence Club at the Hard Rock Hotel. http://lead.vcisa.com/lead/' + codigo_lead
+                        //FIM Mensagem
 
-                    //FIM Mensagem
+                        //URL para envio de SMS
+                        var url_SMS = 'https://api-http.zenvia.com/GatewayIntegration/msgSms.do?dispatch=send&account=venture.smsonline&code=3Xyy5vhKh2&to=55' + numero + '&msg=' + mensagem
+                        //FIM URL para envio de SMS
 
-                    //URL para envio de SMS
-                    var url_SMS = 'https://api-http.zenvia.com/GatewayIntegration/msgSms.do?dispatch=send&account=venture.smsonline&code=3Xyy5vhKh2&to=55' + numero + '&msg=' + mensagem
-                    //FIM URL para envio de SMS
-
-                    $.ajax({
-                        type: 'GET',
-                        url: url_SMS,
-                        beforeSend: function () {
-                            Swal.fire({
-                                title: 'Enviando...',
-                                text: 'Por favor aguarde',
-                                timerProgressBar: true,
-                                onBeforeOpen: () => {
-                                    Swal.showLoading()
-                                }
-                            })
-                        },
-                    }).done(function (resposta) {
-                        if (resposta == '000 - Message Sent') {
-                            Swal.fire({
-                                icon: 'success',
-                                html: '<div class="col-sm-12 mb-3">Mensagem enviada com sucesso</div>' +
-                                    '<div class="col-sm-12" id="qrcode"></div>',// +
+                        $.ajax({
+                            type: 'GET',
+                            url: url_SMS,
+                            beforeSend: function () {
+                                Swal.fire({
+                                    title: 'Enviando...',
+                                    text: 'Por favor aguarde',
+                                    timerProgressBar: true,
+                                    onBeforeOpen: () => {
+                                        Swal.showLoading()
+                                    }
+                                })
+                            },
+                        }).done(function (resposta) {
+                            if (resposta == '000 - Message Sent') {
+                                Swal.fire({
+                                    icon: 'success',
+                                    html: '<div class="col-sm-12 mb-3">Mensagem enviada com sucesso</div>' +
+                                        '<div class="col-sm-12" id="qrcode"></div>',// +
                                     //'<a class="mt-3 btn btn-block btn-success2" href="https://api.whatsapp.com/send?phone=55' + numero + '&amp;text=Ol%C3%A1%20' + nome + ',,%20acesse%20o%20link%20abaixo,%20complete%20seu%20cadastro.%20Viva%20uma%20grande%20experiencia%20com%20o%20Residence%20Club%20at%20the%20Hard%20Rock%20Hotel.%0D%0D%20%20http://lead.vcisa.com/lead/' + localStorage.getItem('id_lead') + '"><img id="whats" src="../../imagens/icon_whatsapp.png" alt=""></a>',
-                                allowOutsideClick: false,
-                                allowEscapeKey: false,
-                                confirmButtonText: 'Próximo',
-                            })
-                            gerar_qrcode(localStorage.getItem('id_lead'))
-                            $('#nome').val('');
-                            $('#celular').val('');
-                        } else {
-                            Swal.fire({
-                                icon: 'error',
-                                text: 'Número de celular incorreto ou incompleto',
-                                allowOutsideClick: false,
-                                allowEscapeKey: false,
-                                confirmButtonText: 'Ok',
-                            })
-                        }
-                    })
-                    break;
+                                    allowOutsideClick: false,
+                                    allowEscapeKey: false,
+                                    confirmButtonText: 'Próximo',
+                                })
+                                gerar_qrcode(localStorage.getItem('id_lead'))
+                                $('#nome').val('');
+                                $('#celular').val('');
+                                document.getElementById("enviar_SMS").disabled = false;
+                            } else {
+                                Swal.fire({
+                                    icon: 'error',
+                                    text: 'Número de celular incorreto ou incompleto',
+                                    allowOutsideClick: false,
+                                    allowEscapeKey: false,
+                                    confirmButtonText: 'Ok',
+                                })
+                            }
+                        })
+                        break;
 
-            }
-        })
+                }
+            })
 
+        }
     }
 })
 
-
-/*Modelo de AJAX
-$.ajax({
-    type: 'POST',
-    url: '/',
-    data: {},
-    dataType: 'json',
-    contentType: 'application/x-www-form-urlencoded',
-
-}).done(function (resposta) {
-
-})
-*/
 
 function gerar_qrcode(id) {
     new QRCode(document.getElementById("qrcode"), {
@@ -396,4 +294,76 @@ function gerar_qrcode(id) {
         width: 150,
         height: 150,
     })
+}
+
+
+function carregar_estados() {
+    //var id_estado = $('#estado').val();
+    $.ajax({
+        type: 'GET',
+        url: '/carregar_estados',
+        dataType: 'json',
+        contentType: 'application/x-www-form-urlencoded',
+
+    }).done(function (resposta) {
+        $(resposta.estados).each(function (index, element) {
+            $('<option>').val(element.id).text(element.estado).appendTo($('#estado'));
+        });
+    })
+}
+
+
+function carregar_cidades() {
+    var id_estado = $('#estado').val();
+
+    $('#cidade').val("").text("");
+    $('<option>').val("").text("Selecione").appendTo('#cidade');
+
+    $.ajax({
+        type: 'GET',
+        url: '/carregar_cidades',
+        dataType: 'json',
+        contentType: 'application/x-www-form-urlencoded',
+
+    }).done(function (resposta) {
+        $(resposta.cidades).each(function (index, element) {
+            // de acordo com o id do estado do select 
+            if (id_estado == element.id_estado) {
+                $('<option>').val(element.id).text(element.cidade).appendTo($('#cidade'));
+            }
+        });
+    })
+}
+
+
+function carregar_paceiros() {
+    var id_estado = $('#estado').val();
+    var id_cidade = $('#cidade').val();
+
+    $('#parceiro').val("").text("");
+    $('<option>').val("").text("Selecione").appendTo('#parceiro');
+
+    $.ajax({
+        type: 'GET',
+        url: '/carregar_parceiros',
+        dataType: 'json',
+        contentType: 'application/x-www-form-urlencoded',
+
+    }).done(function (resposta) {
+        $(resposta.parceiros).each(function (index, element) {
+            // de acordo com o id do estado e da cidade do select 
+            if ((id_estado == element.id_estado) && (id_cidade == element.id_cidade)) {
+                $('<option>').val(element.id).text(element.nome).appendTo($('#parceiro'));
+            }
+        });
+    })
+}
+
+
+function validar_numero_celular(numero) {
+    if (numero.length == 11) {
+        return true
+    } else {
+        return false
+    }
 }
